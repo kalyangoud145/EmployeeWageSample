a905355d9e37982184871447680a792892415cd8
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-21 09:26:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int IS_EMPLOYEE_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(2);
+            if (empCheck == IS_EMPLOYEE_FULL_TIME)
+            {
+                Console.WriteLine("Employee present");
+            }
+            else
+            {
+                Console.WriteLine("Employee absent");
+            }
+        }
+    }
+}
