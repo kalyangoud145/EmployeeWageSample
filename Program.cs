8fd51ca5dd95f05b836bd95e8c730440103e1073
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-21 09:45:56 +0530

    [Kalyan] add . added  funtionality for calculating daily wage

--- Program.cs
+++ Program.cs
@@ -5,17 +5,24 @@ namespace EmployeeWage
     class Program
     {
         static void Main(string[] args)
-        {
+        {///
             const int IS_EMPLOYEE_FULL_TIME = 1;
+            const int WAGE_PER_HR = 20, TOTAL_HRS_IN_A_DAY = 8;
+            int totalEmployeeWage;
+
             Random random = new Random();
             int empCheck = random.Next(2);
+            ///Checks if employee is present, if present it will print
+            ///employee present if condition fails it prints employee absent
             if (empCheck == IS_EMPLOYEE_FULL_TIME)
             {
-                Console.WriteLine("Employee present");
+                totalEmployeeWage = WAGE_PER_HR * TOTAL_HRS_IN_A_DAY;
+                Console.WriteLine("Employee present and daily wage is: " + totalEmployeeWage); 
+
             }
             else
             {
-                Console.WriteLine("Employee absent");
+                Console.WriteLine("Employee absent ");
             }
         }
     }
